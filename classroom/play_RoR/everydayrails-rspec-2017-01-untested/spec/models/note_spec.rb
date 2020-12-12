8800d7adf6666e9e39a2899ba91107569d7dcdb7
nagao <38156084+yasanori@users.noreply.github.com>
2020-12-12 20:03:47 +0900

    scope serach  for note

--- classroom/play_RoR/everydayrails-rspec-2017-01-untested/spec/models/note_spec.rb
+++ classroom/play_RoR/everydayrails-rspec-2017-01-untested/spec/models/note_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe Note, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  # 検索文字列に一致するメモを返すこと
+  it "return notes that match the search term" do
+    user = User.create(
+      first_name: "Katuo",
+      last_name: "Isono",
+      email: "tester@example.com",
+      password: "dottle-nouveau-pavilion-tights-furze"
+    )
+    project = user.projects.create(name: "Test Project")
+
+    note1 = project.notes.create(
+      message: "This is the first note.",
+      user: user
+    )
+    note2 = project.notes.create(
+      message: "This is the second note.",
+      user: user
+    )
+    note3 = project.notes.create(
+      message: "First, preheat the oven.",
+      user: user
+    )
+
+    expect(Note.search("first")).to include(note1, note3)
+    expect(Note.search("first")).to_not include(note2)
+  end
+
 end
