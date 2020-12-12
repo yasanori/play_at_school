33c421b262e26e7590438449cf195be9f1440f3f
nagao <38156084+yasanori@users.noreply.github.com>
2020-12-12 15:40:39 +0900

    first test

--- classroom/play_RoR/everydayrails-rspec-2017-01-untested/spec/models/user_spec.rb
+++ classroom/play_RoR/everydayrails-rspec-2017-01-untested/spec/models/user_spec.rb
@@ -2,7 +2,16 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   # 姓、名、メール、パスワードがあれば有効な状態であること
-  it "is valid with a first name, last name, email, and password"
+  it "is valid with a first name, last name, email, and password" do
+    user = User.new(
+      first_name: "Aaron",
+      last_name: "Sumner",
+      email: "tester@example.com",
+      password: "dottle-nouveau-pavilion-tights-furze"
+    )
+
+    expect(user).to be_valid
+  end
   # 名がなければ無効な状態であること
   it "is invalid without a first name"
   # 姓がなければ無効な状態であること
