4cfe54e2e54dae8c9b797609c2a8e80830d7ef3e
nagao <38156084+yasanori@users.noreply.github.com>
2020-12-13 22:24:02 +0900

    test for projects#create

--- classroom/play_RoR/everydayrails-rspec-2017-01-untested/spec/controllers/projects_controller_spec.rb
+++ classroom/play_RoR/everydayrails-rspec-2017-01-untested/spec/controllers/projects_controller_spec.rb
@@ -70,4 +70,40 @@ RSpec.describe ProjectsController, type: :controller do
       end
     end
   end
+
+  describe "#create" do
+    # 認証済みのユーザーとして
+    context "as an authenticated user" do
+      before do
+        @user = FactoryBot.create(:user)
+      end
+
+      # プロジェクトを追加できること
+      it "adds a project" do
+        project_params = FactoryBot.attributes_for(:project)
+        sign_in @user
+        expect{
+          post :create, params: { project: project_params}
+        }.to change(@user.projects, :count).by(1)
+      end
+    end
+
+    # ゲストとして
+    context "as a guest" do
+      # 302レスポンスを返すこと
+      it "return a 302 response" do
+        project_params = FactoryBot.attributes_for(:project)
+        post :create, params: {project: project_params}
+        expect(response).to have_http_status "302"
+      end
+
+      # サインイン画面にリダイレクトすること
+      it "redirects to the sign-in page" do
+        project_params = FactoryBot.attributes_for(:project)
+        post :create, params: {project: project_params}
+
+        expect(response).to redirect_to "/users/sign_in"
+      end
+    end
+  end
 end
