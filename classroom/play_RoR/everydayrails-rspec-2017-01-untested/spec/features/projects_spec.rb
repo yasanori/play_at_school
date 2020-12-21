c4f13530b05afa1b2f705708f37461c4f9ddc581
nagao <38156084+yasanori@users.noreply.github.com>
2020-12-21 20:51:44 +0900

    sign in をmoduleに切り出し

--- classroom/play_RoR/everydayrails-rspec-2017-01-untested/spec/features/projects_spec.rb
+++ classroom/play_RoR/everydayrails-rspec-2017-01-untested/spec/features/projects_spec.rb
@@ -4,12 +4,7 @@ RSpec.feature "Projects", type: :feature do
   # ユーザーは新しいプロジェクトを作成する
   scenario "user creates a new project" do
     user = FactoryBot.create(:user)
-
-    visit root_path
-    click_link "Sign in"
-    fill_in "Email", with: user.email
-    fill_in "Password",with: user.password
-    click_button "Log in"
+    sign_in_as user
 
     expect{
       click_link "New Project"
