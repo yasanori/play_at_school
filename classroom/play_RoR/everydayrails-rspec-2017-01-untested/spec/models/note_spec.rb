6c089cc4ce2e4a0b471773521b8753cb38a80ad9
nagao <38156084+yasanori@users.noreply.github.com>
2020-12-28 22:51:33 +0900

    instance_double

--- classroom/play_RoR/everydayrails-rspec-2017-01-untested/spec/models/note_spec.rb
+++ classroom/play_RoR/everydayrails-rspec-2017-01-untested/spec/models/note_spec.rb
@@ -25,7 +25,7 @@ RSpec.describe Note, type: :model do
 
   # 名前の取得をメモを作成したユーザーに移譲すること
   it "delegates name to the user who created it" do
-    user = double("user", name: "Fake User")
+    user = instance_double("User", name: "Fake User")
     note = Note.new
     allow(note).to receive(:user).and_return(user)
     expect(note.user_name).to eq "Fake User"
