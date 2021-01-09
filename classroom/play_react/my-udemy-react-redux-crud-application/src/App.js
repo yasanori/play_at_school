8facde8055351a0b16ec5445ebb1a94920590973
nagao <38156084+yasanori@users.noreply.github.com>
2021-01-09 17:18:18 +0900

    study proptypes

--- classroom/play_react/my-udemy-react-redux-crud-application/src/App.js
+++ classroom/play_react/my-udemy-react-redux-crud-application/src/App.js
@@ -1,10 +1,11 @@
+import PropTypes from 'prop-types'
 
 const App = () => {
   const helloWorldJa = <h1>こんにちは,世界。</h1>
   const profiles = [
     { name: "Taro", age: 25 },
     { name: "Hanako", age: 22 },
-    { name: "No name" }
+    { name: "No name", age: 100 }
   ]
   return (
     <>
@@ -18,8 +19,9 @@ const User = (props) => {
   return <div>Hi, I am {props.name} and {props.age} years old.</div>
 }
 
-User.defaultProps = {
-  age: 20
+User.propTypes = {
+  name: PropTypes.string,
+  age: PropTypes.number.isRequired
 }
 
 export default App;
