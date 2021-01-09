1c873b8b7df1203fc05c0a8f46c0375087dee33f
nagao <38156084+yasanori@users.noreply.github.com>
2021-01-09 17:04:36 +0900

    study props

--- classroom/play_react/my-udemy-react-redux-crud-application/src/App.js
+++ classroom/play_react/my-udemy-react-redux-crud-application/src/App.js
@@ -1,9 +1,25 @@
 
-function App() {
+const App = () => {
   const helloWorldJa = <h1>こんにちは,世界。</h1>
+  const profiles = [
+    { name: "Taro", age: 25 },
+    { name: "Hanako", age: 22 },
+    { name: "No name" }
+  ]
   return (
-    <h1>{helloWorldJa}</h1>
+    <>
+      {helloWorldJa}
+      {profiles.map((profile, index) => { return <User name={profile.name} age={profile.age} key={index} /> })}
+    </>
   );
+}
+
+const User = (props) => {
+  return <div>Hi, I am {props.name} and {props.age} years old.</div>
+}
+
+User.defaultProps = {
+  age: 20
 }
 
 export default App;
