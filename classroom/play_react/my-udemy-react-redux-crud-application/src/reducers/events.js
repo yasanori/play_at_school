bccd6c8c5f27b1746861695a237e903da5bb2661
nagao <38156084+yasanori@users.noreply.github.com>
2021-01-11 19:43:30 +0900

    update できたよ

--- classroom/play_react/my-udemy-react-redux-crud-application/src/reducers/events.js
+++ classroom/play_react/my-udemy-react-redux-crud-application/src/reducers/events.js
@@ -1,6 +1,9 @@
 import _ from 'lodash'
 import {
   READ_EVENTS,
+  CREATE_EVENT,
+  READ_EVENT,
+  UPDATE_EVENT,
   DELETE_EVENT
 } from '../actions'
 
@@ -8,6 +11,11 @@ export default (events = {}, action) => {
   switch (action.type) {
     case READ_EVENTS:
       return _.mapKeys(action.response.data, 'id')
+    case READ_EVENT:
+    case CREATE_EVENT:
+    case UPDATE_EVENT:
+      const data = action.response.data
+      return { ...events, [data.id]: data }
     case DELETE_EVENT:
       delete events[action.id]
       return { ...events }
