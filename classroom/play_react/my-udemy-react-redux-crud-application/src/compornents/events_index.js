6d46817c97e065f8f054be6cb076498ecf25881a
nagao <38156084+yasanori@users.noreply.github.com>
2021-01-11 13:25:29 +0900

    event new の準備

--- classroom/play_react/my-udemy-react-redux-crud-application/src/compornents/events_index.js
+++ classroom/play_react/my-udemy-react-redux-crud-application/src/compornents/events_index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import _ from 'lodash'
+import { Link } from 'react-router-dom'
 
 import { readEvents } from '../actions'
 
@@ -20,18 +21,22 @@ class EventsIndex extends Component {
 
   render() {
     return (
-      <table>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Title</th>
-            <th>Body</th>
-          </tr>
-        </thead>
-        <tbody>
-          {this.renderEvents()}
-        </tbody>
-      </table>
+      <>
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Title</th>
+              <th>Body</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.renderEvents()}
+          </tbody>
+        </table>
+
+        <Link to="/events/new">New Event</Link>
+      </>
     );
   }
 }
---
+++ classroom/play_react/my-udemy-react-redux-crud-application/src/compornents/events_new.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
+
+// import { postEvent } from '../actions'
+
+class EventsNew extends Component {
+
+  render() {
+    return (
+      <>
+      </>
+    );
+  }
+}
+
+// const mapDispatchToProps = ({ postEvents })
+
+
+
+export default connect(null, null)(EventsNew)
