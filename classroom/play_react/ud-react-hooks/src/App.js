0f08a7776edc2b79b8c4af9dca1375301baf9e8f
nagao <38156084+yasanori@users.noreply.github.com>
2021-02-01 07:12:31 +0900

    setState

--- classroom/play_react/ud-react-hooks/src/App.js
+++ classroom/play_react/ud-react-hooks/src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
 import Counter from './components/Counter';
 import CounterHooks from './components/CounterHooks';
+import FormHook from './components/FormHook';
+import ItemHook from './components/ItemHook';
+
 
 function App() {
   return (
     <div className="App">
       {/* <Counter /> */}
-      <CounterHooks />
+      {/* <CounterHooks /> */}
+      <FormHook />
+      <ItemHook />
     </div>
   );
 }
---
+++ classroom/play_react/ud-react-hooks/src/components/FormHook.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react'
+
+function FormHook() {
+  const [name, setName] = useState({ firstName: "", lastName: "" })
+  return (
+    <div>
+      <p>First Name: - {name.firstName}</p>
+      <p>Last Name: - {name.lastName}</p>
+      <form>
+        <input
+          type="text"
+          value={name.firstName}
+          onChange={e => setName({ ...name, firstName: e.target.value })}
+        />
+        <input
+          type="text"
+          value={name.lastName}
+          onChange={e => setName({ ...name, lastName: e.target.value })}
+        />
+      </form>
+    </div>
+  )
+}
+
+export default FormHook
