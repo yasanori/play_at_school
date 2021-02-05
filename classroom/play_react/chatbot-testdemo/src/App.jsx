ea20125ae80be59a6bedee3b124f57a6ca6ed7cc
nagao <38156084+yasanori@users.noreply.github.com>
2021-02-06 08:55:29 +0900

    chat after icon

--- classroom/play_react/chatbot-testdemo/src/App.jsx
+++ classroom/play_react/chatbot-testdemo/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import defaultDataset from "./dataset"
 import './assets/styles/style.css'
-import { AnswersList, Answer } from "./components/index";
+import { AnswersList, Chats } from "./components/index";
 
 export default class App extends React.Component {
   constructor(props) {
@@ -24,14 +24,32 @@ export default class App extends React.Component {
     })
   }
 
+  initChats = () => {
+    const initDataset = this.state.dataset[this.state.currentId];
+    const chat = {
+      text: initDataset.question,
+      type: 'question'
+    }
+
+    const chats = this.state.chats;
+    chats.push(chat)
+
+
+    this.setState({
+      chats: chats
+    })
+  }
+
   componentDidMount() {
-    this.initAnswer()
+    this.initAnswer();
+    this.initChats();
   }
 
   render() {
     return (
       <div className="c-section">
         <div className="c-box">
+          <Chats chats={this.state.chats} />
           <AnswersList answers={this.state.answers} />
         </div>
       </div>
---
+++ classroom/play_react/chatbot-testdemo/src/components/Chat.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import ListItem from '@material-ui/core/ListItem';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Avatar from '@material-ui/core/Avatar';
+import Me from '../assets/img/IMG_6429.png'
+import You from '../assets/img/IMG_6431.png'
+
+const Chat = (props) => {
+  const isQuestion = (props.type === 'question')
+  const classes = isQuestion ? 'p-chat_row' : 'p-chat_reverse'
+
+  return (
+    <ListItem className={classes}>
+      <ListItemAvatar>
+        {isQuestion ? (
+          <Avatar alt="icon" src={Me} />
+        ) : (
+            <Avatar alt="icon" src={You} />
+          )}
+      </ListItemAvatar>
+      <div className="p-chat__bubble">
+        {props.text}
+      </div>
+    </ListItem>
+  )
+}
+
+export default Chat
