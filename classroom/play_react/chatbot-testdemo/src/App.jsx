2d9c886f4748a332a12480ae139cb924580f9aa1
nagao <38156084+yasanori@users.noreply.github.com>
2021-02-06 11:42:08 +0900

    slack 連携

--- classroom/play_react/chatbot-testdemo/src/App.jsx
+++ classroom/play_react/chatbot-testdemo/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import defaultDataset from "./dataset"
 import './assets/styles/style.css'
 import { AnswersList, Chats } from "./components/index";
+import FormDialog from './components/Forms/FormDialog';
 
 export default class App extends React.Component {
   constructor(props) {
@@ -14,6 +15,8 @@ export default class App extends React.Component {
       open: false
     }
     this.selectAnswer = this.selectAnswer.bind(this)
+    this.handleClickOpen = this.handleClickOpen.bind(this)
+    this.handleClose = this.handleClose.bind(this);
   }
 
   displayNextQuestion = (nextQuestionId) => {
@@ -32,8 +35,19 @@ export default class App extends React.Component {
   selectAnswer = (selectedAnswer, nextQuestionId) => {
     switch (true) {
       case (nextQuestionId === 'init'):
-        this.displayNextQuestion(nextQuestionId)
+        setTimeout(() => {
+          this.displayNextQuestion(nextQuestionId)
+        }, 500);
         break;
+      case (nextQuestionId === "contact"):
+        this.handleClickOpen();
+        break;
+      case (/https:*/.test(nextQuestionId)):
+        const a = document.createElement('a')
+        a.href = nextQuestionId;
+        a.target = '_blank';
+        a.click();
+        break
       default:
         const chats = this.state.chats;
         chats.push({
@@ -44,11 +58,20 @@ export default class App extends React.Component {
         this.setState({
           chats: chats
         })
-        this.displayNextQuestion(nextQuestionId)
+        setTimeout(() => {
+          this.displayNextQuestion(nextQuestionId)
+        }, 1000);
         break;
     }
   }
 
+  handleClickOpen = () => {
+    this.setState({ open: true });
+  };
+
+  handleClose = () => {
+    this.setState({ open: false })
+  };
 
   componentDidMount() {
     const initAnswer = "";
@@ -68,6 +91,7 @@ export default class App extends React.Component {
         <div className="c-box">
           <Chats chats={this.state.chats} />
           <AnswersList answers={this.state.answers} select={this.selectAnswer} />
+          <FormDialog open={this.state.open} handleClose={this.handleClose} />
         </div>
       </div>
     );
