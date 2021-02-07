1f867b1a861f501390a6ee41468188ef41a990ed
nagao <38156084+yasanori@users.noreply.github.com>
2021-02-07 10:29:27 +0900

    firestore から取得

---
+++ classroom/play_react/chatbot-testdemo/src/firebase/config.js
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBUIWNP5A6YbiGqBajlXfB95Kz4MK48Cjo",
+  authDomain: "chatbot-testdemo.firebaseapp.com",
+  projectId: "chatbot-testdemo",
+  storageBucket: "chatbot-testdemo.appspot.com",
+  messagingSenderId: "760984554113",
+  appId: "1:760984554113:web:065128d3aa7bde45e0db80",
+  measurementId: "G-EDDTPEJHJE"
+};
+
+export default firebaseConfig;
