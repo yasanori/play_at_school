c62154771898a5985d8be35ed16f05d62602a157
nagao <38156084+yasanori@users.noreply.github.com>
2021-02-09 12:49:06 +0900

    reducer

---
+++ classroom/play_react/ec-redux-demo-app/src/reducks/users/reducers.js
@@ -0,0 +1,12 @@
+import * as Actions from './actions';
+import initialState from '../store/initialState';
+
+export const UserReducer = (state = initialState.users, action) => {
+  switch (action.type) {
+    case Actions.SIGN_IN:
+      return {
+        ...state,
+        ...action.payload
+      }
+  }
+}
