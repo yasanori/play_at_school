58251b89aa092d0e9e21d76c21a2182ab040c1b3
nagao <38156084+yasanori@users.noreply.github.com>
2021-02-13 10:43:51 +0900

    style components作りました

--- classroom/play_react/uber-eats-like-pra/frontend/src/containers/Restaurants.jsx
+++ classroom/play_react/uber-eats-like-pra/frontend/src/containers/Restaurants.jsx
@@ -1,5 +1,29 @@
 import React, { useEffect } from 'react'
+import styled from 'styled-components';
+
 import { fetchRestaurants } from '../apis/restaurants';
+
+// レストラン画像
+import MainLogo from '../images/logo.png';
+import MainCoverImage from '../images/main-cover-image.png'
+
+const HeaderWrapper = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  padding: 8px 32px;
+`;
+const MainLogoImage = styled.img`
+  height: 90px;
+`;
+
+const MainCoverImageWrapper = styled.div`
+  text-align: center;
+`;
+
+const MainCover = styled.img`
+  height: 600px;
+`;
+
 
 export const Restaurants = () => {
 
@@ -10,7 +34,12 @@ export const Restaurants = () => {
 
   return (
     <>
-      レストラン
+      <HeaderWrapper>
+        <MainLogoImage src={MainLogo} alt="main logo" />
+      </HeaderWrapper>
+      <MainCoverImageWrapper>
+        <MainCover src={MainCoverImage} alt="main cover" />
+      </MainCoverImageWrapper>
     </>
   )
 }
