95f29aece5259d998ce8a611f78528d7d020436e
nagao <38156084+yasanori@users.noreply.github.com>
2021-03-06 11:26:24 +0900

    firebase 準備

---
+++ classroom/play_react/ec-redux-demo-app/src/firebase/config.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+  apiKey: "AIzaSyCrzNVU2qaTAfnyZfuJlAg3y5hXMk9cpRk",
+  authDomain: "ec-redux-demo.firebaseapp.com",
+  projectId: "ec-redux-demo",
+  storageBucket: "ec-redux-demo.appspot.com",
+  messagingSenderId: "396367101345",
+  appId: "1:396367101345:web:248363ed5d0804e0991fe0",
+  measurementId: "G-60HN2NRMWB"
+};
