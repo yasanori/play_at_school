107b3dc94d6dc9307e5d9d9f11061a878a26fc59
nagao <38156084+yasanori@users.noreply.github.com>
2021-03-07 10:15:56 +0900

    signUp関数作成

--- classroom/play_react/ec-redux-demo-app/src/reducks/users/operations.js
+++ classroom/play_react/ec-redux-demo-app/src/reducks/users/operations.js
@@ -1,5 +1,6 @@
 import { signInAction } from './actions';
 import { push } from 'connected-react-router';
+import { auth, FirebaseTimestamp, db } from '../../firebase/index';
 
 export const signIn = () => {
   return async (dispatch, getState) => {
@@ -24,4 +25,40 @@ export const signIn = () => {
     }
   }
 
+}
+
+export const signUp = (username, email, password, confirmPassword) => {
+
+  return async (dispatch) => {
+    if (username === "" || email === "" || password === "" || confirmPassword === "") {
+      alert("必須項目が未入力です")
+      return false
+    }
+    if (password !== confirmPassword) {
+      alert("パスワードが一致しません")
+      return false
+    }
+    return auth.createUserWithEmailAndPassword(email, password)
+      .then(result => {
+        const user = result.user
+        if (user) {
+          const uid = user.uid;
+          const timestamp = FirebaseTimestamp.now();
+
+          const userInitialData = {
+            created_at: timestamp,
+            email: email,
+            role: "costomer",
+            uid: uid,
+            updated_at: timestamp,
+            username: username
+          }
+
+          db.collection('users').doc(uid).set(userInitialData)
+            .then(() => {
+              dispatch(push('/'))
+            })
+        }
+      })
+  }
 }
--- classroom/play_react/ec-redux-demo-app/src/templates/SignUp.jsx
+++ classroom/play_react/ec-redux-demo-app/src/templates/SignUp.jsx
@@ -1,7 +1,12 @@
 import React, { useCallback, useState } from 'react'
-import { TextInput } from '../components/Uikit';
+import { useDispatch } from 'react-redux';
+
+import { TextInput, PrimaryButton } from '../components/Uikit';
+import { signUp } from '../reducks/users/operations';
 
 const SignUp = () => {
+
+  const dispatch = useDispatch()
 
   const [username, setUsername] = useState("")
   const [email, setEmail] = useState("")
@@ -27,46 +32,52 @@ const SignUp = () => {
   return (
     <div className="c-section-container">
       <h2 className="u-text__headline u-text-center">アカウント登録</h2>
-      <div className="module-spacer--medium">
-        <TextInput
-          fullWidth={true}
-          label={"ユーザー名"}
-          multiline={false}
-          required={true}
-          rows={1}
-          value={username}
-          type={"text"}
-          onChange={inputUsername}
-        />
-        <TextInput
-          fullWidth={true}
-          label={"メールアドレス"}
-          multiline={false}
-          required={true}
-          rows={1}
-          value={email}
-          type={"email"}
-          onChange={inputEmail}
-        />
-        <TextInput
-          fullWidth={true}
-          label={"パスワード"}
-          multiline={false}
-          required={true}
-          rows={1}
-          value={password}
-          type={"password"}
-          onChange={inputPassword}
-        />
-        <TextInput
-          fullWidth={true}
-          label={"パスワード（確認）"}
-          multiline={false}
-          required={true}
-          rows={1}
-          value={confirmPassword}
-          type={"password"}
-          onChange={inputConfirmPassword}
+      <div className="module-spacer--medium" />
+      <TextInput
+        fullWidth={true}
+        label={"ユーザー名"}
+        multiline={false}
+        required={true}
+        rows={1}
+        value={username}
+        type={"text"}
+        onChange={inputUsername}
+      />
+      <TextInput
+        fullWidth={true}
+        label={"メールアドレス"}
+        multiline={false}
+        required={true}
+        rows={1}
+        value={email}
+        type={"email"}
+        onChange={inputEmail}
+      />
+      <TextInput
+        fullWidth={true}
+        label={"パスワード"}
+        multiline={false}
+        required={true}
+        rows={1}
+        value={password}
+        type={"password"}
+        onChange={inputPassword}
+      />
+      <TextInput
+        fullWidth={true}
+        label={"パスワード（確認）"}
+        multiline={false}
+        required={true}
+        rows={1}
+        value={confirmPassword}
+        type={"password"}
+        onChange={inputConfirmPassword}
+      />
+      <div className="module-spacer--medium" />
+      <div className="center">
+        <PrimaryButton
+          label={"アカウントを登録する"}
+          onClick={() => dispatch(signUp(username, email, password, confirmPassword))}
         />
       </div>
     </div>
