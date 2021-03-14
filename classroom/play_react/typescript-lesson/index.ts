a2cc95eb11673b90abf39aa22299b6a54f8fb4c2
nagao <38156084+yasanori@users.noreply.github.com>
2021-03-14 15:43:31 +0900

    boolean number string

--- classroom/play_react/typescript-lesson/index.ts
+++ classroom/play_react/typescript-lesson/index.ts
@@ -1,2 +1,7 @@
-let hello: string = 'hello';
-console.log(hello);
+let hasValue: boolean = true
+let count: number = 10;
+let float: number = 3.14;
+let negative: number = -0.1;
+let single: string = 'hello';
+let double: string = "hello";
+let back: string = `hello`;
