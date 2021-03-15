e4e48fa0de2b37d2f70d547fc9e1de4f09e3b947
nagao <38156084+yasanori@users.noreply.github.com>
2021-03-15 10:16:44 +0900

    変数に型

--- classroom/play_react/typescript-lesson/index.js
+++ classroom/play_react/typescript-lesson/index.js
@@ -1,2 +1,33 @@
-var hello = 'hello';
-console.log(hello);
+var hasValue = true;
+var count = 10;
+var float = 3.14;
+var negative = -0.1;
+var single = 'hello';
+var double = "hello";
+var back = "hello";
+var person = {
+    name: "nagao",
+    age: 25
+};
+var fruits = ["apple", "banana", "grape"];
+var fruit = fruits[0];
+//tuple
+var book = ["manga", 480, true];
+// pushはできる
+// book.push(false)
+// 参照はできない
+// console.log(book[3])
+// Enum 列挙型
+var CoffeSize;
+(function (CoffeSize) {
+    CoffeSize["short"] = "short";
+    CoffeSize["tall"] = "tall";
+    CoffeSize["grande"] = "grande";
+    CoffeSize["venti"] = "venti";
+})(CoffeSize || (CoffeSize = {}));
+var coffe = {
+    hot: true,
+    size: CoffeSize.tall
+};
+// エラー
+// coffe.size = "short"
--- classroom/play_react/typescript-lesson/index.ts
+++ classroom/play_react/typescript-lesson/index.ts
@@ -5,3 +5,59 @@ let negative: number = -0.1;
 let single: string = 'hello';
 let double: string = "hello";
 let back: string = `hello`;
+
+const person: {
+  name: string,
+  age: number
+} = {
+  name: "nagao",
+  age: 25
+}
+
+const fruits: string[] = ["apple", "banana", "grape"]
+const fruit = fruits[0]
+
+//tuple
+const book: [string, number, boolean] = ["manga", 480, true]
+// pushはできる
+// book.push(false)
+// 参照はできない
+// console.log(book[3])
+
+
+// Enum 列挙型
+
+enum CoffeSize {
+  SHORT,
+  TALL,
+  GRANDE,
+  VENTI
+}
+
+
+const coffe = {
+  hot: true,
+  size: CoffeSize.TALL
+}
+
+// エラー
+// coffe.size = "SHORT"
+
+// anyが絡むと型づけが完全に無効となる
+let anything: any = true;
+anything = 'hello';
+anything.aaa = {}
+
+// string型にも代入できてしまう
+let strBanana = "banana"
+strBanana = anything;
+
+// union
+let unionTyoes: (number | string)[] = ["string", 10]
+let unionType: number | string = 10;
+// number型で使用できないメソッドでエラー
+// unionTyoe.toUpperCase
+
+// string型を代入すれば使用できる
+unionType = "String"
+unionType.toUpperCase()
