bc20a9ce232f4c48fbfc09e310b0bad8caa4780f
nagao <38156084+yasanori@users.noreply.github.com>
2021-04-25 14:31:29 +0900

    primitive型

---
+++ classroom/play_js/ts-basic-tora/src/basic/any.ts
@@ -0,0 +1,7 @@
+export default function anySample() {
+  let name: any = "nagao";
+  console.log("any sample 1 :", typeof (name), name)
+
+  name = 25
+  console.log("any sample 2 :", typeof (name), name)
+}
--- classroom/play_js/ts-basic-tora/src/basic/primitive.ts
+++ classroom/play_js/ts-basic-tora/src/basic/primitive.ts
@@ -1,4 +1,13 @@
 export default function primitiveSample() {
   let name: string = "nagao"
-  console.log("primitive sample 1 :", typeof name, name)
+  console.log("primitive sample 1 :", typeof (name), name)
+
+  let age: number = 25
+  console.log("primitive sample 2 :", typeof (age), age)
+
+  let isSingle: boolean = true
+  console.log("primitive sample 3 :", typeof (isSingle), isSingle)
+
+  const isOver20: boolean = age >= 20
+  console.log("primitive sample 4 :", typeof (isOver20), isOver20)
 }
