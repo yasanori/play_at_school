380c0841fe288f996fa9f8fb2f933f72e1d2897f
yasanori <itsonlf2525@gmail.com>
2021-06-20 16:41:09 +0900

    deploy

--- classroom/play_react/ts_05_30/src/components/TweetInput.tsx
+++ classroom/play_react/ts_05_30/src/components/TweetInput.tsx
@@ -7,7 +7,7 @@ import { Avatar, Button, IconButton } from '@material-ui/core';
 import firebase from 'firebase';
 import AddAPhotoIcon from "@material-ui/icons/AddAPhoto";
 
-const TweetInput= () => {
+const TweetInput: React.FC = () => {
   const user = useSelector(selectUser)
   const [ tweetImage, setTweetImage ] = useState<File | null>(null)
   const [ tweetMsg, setTweetMsg ] = useState("")
